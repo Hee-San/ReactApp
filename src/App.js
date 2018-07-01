314905000a25276437085203c1d2c326c0264da1
へーさん <hira9603859504@gmail.com>
2018-07-02 00:06:09 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import P5Wrapper from 'react-p5-wrapper';
-import sketch from './sketches/sketch';
 
 class App extends Component {
   render() {
@@ -15,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <P5Wrapper sketch=sketch/>
+        <P5Wrapper sketch='./sketches/sketch' />
       </div>
     );
   }
--- src/sketches/sketch.js
+++ src/sketches/sketch.js
@@ -1,33 +1,3 @@
-// M_2_3_01
-//
-// Generative Gestaltung – Creative Coding im Web
-// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
-// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
-// with contributions by Joey Lee and Niels Poldervaart
-// Copyright 2018
-//
-// http://www.generative-gestaltung.de
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * draws an amplitude modulated oscillator
- *
- * KEYS
- * i                 : toggle draw info signal
- * c                 : toggle draw carrier signal
- * 1/2               : info signal frequency -/+
- * arrow left/right  : info signal phi -/+
- * 7/8               : carrier signal frequency -/+ (modulation frequency)
- * s                 : save png
- */
 'use strict';
 
 var sketch = function( p ) {
@@ -95,27 +65,4 @@ var sketch = function( p ) {
     }
     p.endShape();
   };
-
-  p.keyPressed = function() {
-    if (p.key == 's' || p.key == 'S') p.saveCanvas(gd.timestamp(), 'png');
-
-    if (p.key == 'i' || p.key == 'I') drawFrequency = !drawFrequency;
-    if (p.key == 'c' || p.key == 'C') drawModulation = !drawModulation;
-
-    if (p.key == '1') freq--;
-    if (p.key == '2') freq++;
-    freq = p.max(freq,1);
-
-    if (p.keyCode == p.LEFT_ARROW) phi -= 15;
-    if (p.keyCode == p.RIGHT_ARROW) phi += 15;
-
-    if (p.key == '7') modFreq--;
-    if (p.key == '8') modFreq++;
-    modFreq = p.max(modFreq,1);
-
-    console.log("freq: " + freq + ", phi: " + phi + ", modFreq: " + modFreq);
-  };
-
 };
-
-var myp5 = new p5(sketch);
